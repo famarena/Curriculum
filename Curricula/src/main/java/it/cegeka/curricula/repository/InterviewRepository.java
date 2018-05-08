887764692545b01bb811a333949ebb9cd8525055
Francesca Amarena <francescaamarena@hotmail.com>
2018-05-08 15:03:34 +0200

    ff

---
+++ Curricula/src/main/java/it/cegeka/curricula/repository/InterviewRepository.java
@@ -0,0 +1,9 @@
+package it.cegeka.curricula.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.cegeka.curricula.entities.Interview;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+}
