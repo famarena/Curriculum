723c16a43e0fe08f2a6df02c09be801703caf8cb
Francesca Amarena <francescaamarena@hotmail.com>
2018-05-09 10:35:49 +0200

    cc

--- Curricula/src/main/java/it/cegeka/curricula/CurriculaApplication.java
+++ Curricula/src/main/java/it/cegeka/curricula/CurriculaApplication.java
@@ -2,7 +2,7 @@ package it.cegeka.curricula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//c
 @SpringBootApplication
 public class CurriculaApplication {
 
