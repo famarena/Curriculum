adb8f3b886dc242244f894894b751ef0abb0b703
Francesca Amarena <francescaamarena@hotmail.com>
2018-05-09 09:51:43 +0200

    commento

---
+++ Curricula/src/main/java/it/cegeka/curricula/controllers/JobPositionController.java
@@ -0,0 +1,47 @@
+package it.cegeka.curricula.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import it.cegeka.curricula.enums.Sector;
+import it.cegeka.curricula.repository.CandidateRepository;
+import it.cegeka.curricula.service.PositionService;
+import it.cegeka.curricula.service.utility.SkillValue;
+
+public class JobPositionController {
+
+	@Autowired 
+	CandidateRepository candidateRepo;
+	
+	
+	@Autowired 
+	PositionService positionServ;
+	
+	//jefjefn
+	
+	@GetMapping(path="/newPosition")
+	@ResponseBody
+	public String newPosition() {
+		SkillValue s1 = new SkillValue();
+		SkillValue s2 = new SkillValue();
+		SkillValue s3 = new SkillValue();
+		s1.setSkillName("Java");
+		s1.setValue(3);
+		s2.setSkillName("MySQL");
+		s2.setValue(3);
+		s3.setSkillName("C");
+		s3.setValue(5);
+		List<SkillValue> reqSkills = new ArrayList<>();
+		reqSkills.add(s1);
+		reqSkills.add(s2);
+		reqSkills.add(s3);
+
+		positionServ.newPosition(reqSkills, Sector.IT, "c++ dev");
+		return "all";
+	}
+	
+}
